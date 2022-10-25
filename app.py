d6107f8849894195c8025478eb48ee14703cb4b2
Duc Duong <duongduc98@gmail.com>
2022-10-26 00:21:00 +0700

    import sklearn.ensemble.GradientBoostingClassifier

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import logging
 import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
+import sklearn.ensemble.GradientBoostingClassifier
 
 app = Flask(__name__)
 LOG = create_logger(app)
