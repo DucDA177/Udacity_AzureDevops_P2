523a7594c58c7f41063864b46bf80f4850556bcf
Duong Anh Duc <duongduc98@gmail.com>
2022-10-25 13:19:29 +0700

    fix bugs

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def predict():
 
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
-    except: # pylint: disable=W0702
+    except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
